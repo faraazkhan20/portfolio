f2ad7fa867a048b92756246ebaa573fa1a5edc8f
Faraaz Khan <faraazkhan.me@gmail.com>
2025-08-03 23:37:04 +0530

    Refactor: src/assets and src/styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
+    <div className="container p-2">
+      <div className="row">
+        <Hero />
+      </div>
+    </div>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,12 @@
-const borderStyles = {
-  border: "1px solid red",
-};
-
 function Hero() {
   return (
-    <div className="container text-center py-5" style={borderStyles}>
-      <div className="fs-1">Faraaz Khan</div>
-      <div>Junior Software Developer</div>
+    <div className="col-lg-12 mt-5 p-5 text-center outline test">
+      <div className="fs-1 fw-semibold" style={{ letterSpacing: "-2px" }}>
+        The Foundation for your Design System
+      </div>
+      <div className="fs-4 fw-semibold pb-2">Junior Software Developer</div>
       <p>I am a skilled and innovative developer with a passion for crafting efficient and user-centric solutions. With a strong foundation in web development and a commitment to excellence, I continuously strive to deliver impactful projects that blend creativity with functionality</p>
-      <button className="btn btn-primary btn-lg">Click Me</button>
+      <button className="btn btn-sm btn-outline-primary btn-lg">Click Me</button>
     </div>
   );
 }
