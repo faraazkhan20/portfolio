b5e6b804fe914225ebb5d7b94f09a759bd9c8769
Faraaz Khan <faraazkhan.me@gmail.com>
2025-08-13 22:33:10 +0530

    Updated Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,36 +2,42 @@ function Skills() {
   return (
     <div className="mt-5">
       <h5 className="fw-semibold">Skills</h5>
-      <div className="my-2">Here are some of the skills I have acquired over the years of my journey in the tech industry. I am always looking to learn more and improve my skillset.</div>
+      {/* <div className="my-2">Here are some of the skills I have acquired over the years of my journey in the tech industry. I am always looking to learn more and improve my skillset.</div> */}
+      <div className="my-2">Over the years in the tech industry, I have cultivated a diverse range of skills through hands-on experience, continuous learning, and tackling challenging projects.</div>
       <div>
         <div className="mb-1">
           <div className="d-flex flex-wrap gap-1">
             <div className="fw-semibold">1. Languages</div>
+            <span className="badge border border-secondary">C++</span>
             <span className="badge border border-secondary">JavaScript</span>
-            <span className="badge border border-secondary">TypeScript</span>
+            {/* <span className="badge border border-secondary">TypeScript</span> */}
             <span className="badge border border-secondary">Python</span>
-            <span className="badge border border-secondary">Go</span>
+            {/* <span className="badge border border-secondary">Go</span> */}
             <span className="badge border border-secondary">SQL</span>
           </div>
         </div>
         <div className="mb-1">
           <div className="d-flex flex-wrap gap-1">
             <div className="fw-semibold">2. Frontend</div>
+            <span className="badge border border-secondary">HTML5</span>
+            <span className="badge border border-secondary">CSS3</span>
+            <span className="badge border border-secondary">Bootstrap v5.3</span>
             <span className="badge border border-secondary">React.js</span>
-            <span className="badge border border-secondary">Next.js</span>
+            {/* <span className="badge border border-secondary">Next.js</span> */}
             <span className="badge border border-secondary">Tailwind CSS</span>
-            <span className="badge border border-secondary">Shadcn</span>
-            <span className="badge border border-secondary">Tanstack Query</span>
+            {/* <span className="badge border border-secondary">Shadcn</span> */}
+            {/* <span className="badge border border-secondary">Tanstack Query</span> */}
           </div>
         </div>
         <div className="mb-1">
           <div className="d-flex flex-wrap gap-1">
             <div className="fw-semibold">3. Backend</div>
-            <span className="badge border border-secondary">Hono.js</span>
+            <span className="badge border border-secondary">Node.js</span>
             <span className="badge border border-secondary">Express.js</span>
-            <span className="badge border border-secondary">FastAPI</span>
-            <span className="badge border border-secondary">Langchain</span>
-            <span className="badge border border-secondary">AI SDK</span>
+            {/* <span className="badge border border-secondary">Hono.js</span> */}
+            {/* <span className="badge border border-secondary">FastAPI</span> */}
+            {/* <span className="badge border border-secondary">Langchain</span> */}
+            {/* <span className="badge border border-secondary">AI SDK</span> */}
           </div>
         </div>
         <div className="mb-1">
@@ -40,13 +46,13 @@ function Skills() {
             <span className="badge border border-secondary">MongoDB</span>
             <span className="badge border border-secondary">PostgreSQL</span>
             <span className="badge border border-secondary">MySQL</span>
-            <span className="badge border border-secondary">Clickhouse</span>
-            <span className="badge border border-secondary">Redis</span>
-            <span className="badge border border-secondary">Pinecone</span>
-            <span className="badge border border-secondary">Qdrant</span>
+            {/* <span className="badge border border-secondary">Clickhouse</span> */}
+            {/* <span className="badge border border-secondary">Redis</span> */}
+            {/* <span className="badge border border-secondary">Pinecone</span> */}
+            {/* <span className="badge border border-secondary">Qdrant</span> */}
           </div>
         </div>
-        <div className="mb-1">
+        {/* <div className="mb-1">
           <div className="d-flex flex-wrap gap-1">
             <div className="fw-semibold">5. DevOps & Cloud</div>
             <span className="badge border border-secondary">Docker</span>
@@ -55,26 +61,28 @@ function Skills() {
             <span className="badge border border-secondary">AWS</span>
             <span className="badge border border-secondary">Cloudflare</span>
           </div>
-        </div>
-        <div className="mb-1">
+        </div> */}
+        {/* <div className="mb-1">
           <div className="d-flex flex-wrap gap-1">
             <div className="fw-semibold">6. Data Engineering</div>
             <span className="badge border border-secondary">Apache Spark</span>
             <span className="badge border border-secondary">Apache Kafka</span>
             <span className="badge border border-secondary">Databricks</span>
           </div>
-        </div>
+        </div> */}
         <div className="mb-1">
           <div className="d-flex flex-wrap gap-1">
             <div className="fw-semibold">7. Others</div>
             <span className="badge border border-secondary">Git</span>
-            <span className="badge border border-secondary">REST</span>
-            <span className="badge border border-secondary">gRPC</span>
-            <span className="badge border border-secondary">tRPC</span>
-            <span className="badge border border-secondary">Prisma</span>
-            <span className="badge border border-secondary">Drizzle</span>
-            <span className="badge border border-secondary">SQLAlchemy</span>
-            <span className="badge border border-secondary">NixOS</span>
+            <span className="badge border border-secondary">GitHub</span>
+            <span className="badge border border-secondary">Postman</span>
+            {/* <span className="badge border border-secondary">REST</span> */}
+            {/* <span className="badge border border-secondary">gRPC</span> */}
+            {/* <span className="badge border border-secondary">tRPC</span> */}
+            {/* <span className="badge border border-secondary">Prisma</span> */}
+            {/* <span className="badge border border-secondary">Drizzle</span> */}
+            {/* <span className="badge border border-secondary">SQLAlchemy</span> */}
+            {/* <span className="badge border border-secondary">NixOS</span> */}
           </div>
         </div>
       </div>
